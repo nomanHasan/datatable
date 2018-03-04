e66010c3584817a11cf3c92c34aac10676b4f71a
Noman Hasan <nomanbinhussein@gmail.com>
2018-03-05 00:39:15 +0600

    Structuing Table HTML

---
+++ ng-datatable/src/app/datatable/datatable.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'no-datatable',
+  templateUrl: './datatable.component.html',
+  styleUrls: ['./datatable.component.scss']
+})
+export class DatatableComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
