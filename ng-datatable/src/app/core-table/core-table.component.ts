c30c6135b581fb34cf6874c0e3762d57610e1027
Noman Hasan <nomanbinhussein@gmail.com>
2018-05-14 13:42:43 +0600

    Heavy Randomize

--- ng-datatable/src/app/core-table/core-table.component.ts
+++ ng-datatable/src/app/core-table/core-table.component.ts
@@ -11,7 +11,7 @@ import * as localforage from 'localforage';
 export class CoreTableComponent implements OnInit {
 
 
-  rowNumber = 1000;
+  rowNumber = 55;
   columnNumber = 1000;
 
   combinedData;
--- ng-datatable/src/app/table-data/table-data.model.ts
+++ ng-datatable/src/app/table-data/table-data.model.ts
@@ -1,5 +1,50 @@
-import * as randomWords from 'random-words';
+// import * as randomWords from 'random-words';
 // const randomWords = () => 'CELL';
+
+
+
+
+const word = (length = 7) => {
+
+    if (!length) {
+        length = num(10);
+    }
+
+    const vowels = 'aeiou';
+    const constants = 'qwrtpsdfghjklzxcvbnm';
+
+    let text = '';
+
+    Array(length)
+        .fill(0)
+        .forEach(element => {
+            text += constants.includes(text[text.length - 1])
+                ? (Math.random() > 0.8
+                    ? letter(constants)
+                    : letter(vowels))
+                : ((Math.random() > 0.45)
+                    ? letter(vowels)
+                    : letter(constants));
+        });
+
+    text = text
+        .slice(0, 1)
+        .toUpperCase() + text.slice(1);
+    return text;
+};
+
+const num = (max, min = 0) => {
+    return Math.floor(Math.random() * (max - min + 1) + min);
+};
+
+const letter = string => {
+    return string[num(string.length - 1)];
+};
+
+
+
+
+const randomWords = word;
 
 const range = (length) => {
     return Array.from(Array(length).fill(0));
@@ -45,6 +90,8 @@ const getTableData = (row, col) => {
     });
     return {tableData, columns: cols};
 };
+
+
 
 
 export const TableData = {
